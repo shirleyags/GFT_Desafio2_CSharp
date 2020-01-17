7cb1acae693ff4fdea77d64727f68ec21f0a70fd
Shirley Alice <41222167+shirleyags@users.noreply.github.com>
2020-01-17 14:14:05 -0300

    Add files via upload
    Projeto sobre herança e classe abstrata CSharp.

---
+++ Veiculos/Veiculos/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Veiculos.VeiculosTipo;
+
+namespace Veiculos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Carro carro = new Carro();
+            
+
+            Console.WriteLine(carro.Modelo);
+            Console.WriteLine(carro.Velocidade);
+            Console.WriteLine(carro.Passageiros);
+
+
+            Aviao aviao = new Aviao();
+            aviao.Modelo = "boing";
+            aviao.Velocidade = 1000;
+            aviao.Passageiros = 100;
+
+            Console.WriteLine(aviao.Modelo);
+            Console.WriteLine(aviao.Velocidade);
+            Console.WriteLine(aviao.Passageiros);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Veiculos/Veiculos/VeiculosTipo/Veiculo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Veiculos.VeiculosTipo
+{
+    public abstract class Veiculo
+    {
+       
+        public string Modelo { get; set; }
+        public int Velocidade { get; set; }
+        public int Passageiros { get; set; }
+        public bool Combustivel { get; set; }
+
+    }
+}
